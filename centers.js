2cc71580908c0a002aad4df2643c059dca478e88
jjin3633 <jjin3633@github.com>
2026-04-01 12:33:12 +0900

    first commit

---
+++ centers.js
@@ -0,0 +1,13 @@
+const CENTERS = [
+  {
+    name: "문핏",
+    address: "서울 마포구 월드컵북로 434 B1 103호",
+    phone: "0507-1307-1256",
+    instagram: "https://www.instagram.com/moonfit.dmc",
+    website: "https://blog.naver.com/moonfitdmc",
+    lat: 37.5815659,
+    lng: 126.8860337,
+    placeId: "1164043656",
+    image: "",
+  },
+];
